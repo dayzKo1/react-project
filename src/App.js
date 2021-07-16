7101d599b1377bb7d7e9358d6c25ff3ef26716b8
dayzKo1 <845003566@qq.com>
2021-07-16 19:57:16 +0800

    2021-7-16

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
     const { nowpages } = this.state;
     return (
       <div>
-        <Header1231
+        <Header
           pagesClick={this.pagesClick}
           nowpages={nowpages}
         ></Header>
