501b87a056ec9bf4dd1d38d06bc9fd1c89dbba5d
dayzKo1 <845003566@qq.com>
2021-07-16 20:00:32 +0800

    2021-7-16 20:00

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -5,7 +5,7 @@ import { faUsers, faStar, faShareAlt, faExclamation, } from "@fortawesome/free-s
 import "@/css/MyCard.css";
 export default class MyCard extends Component {
   openNewPage = () => {
-    window.open(this.props.htmlUrl);
+    wi123ndow.open(this.props.htmlUrl);
   };
   render() {
     const {
